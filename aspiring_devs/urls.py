d585a4e0c264af1af7d2272a398b03dcd6f5f911
JoseCastanoCoding <j.albertocast@gmail.com>
2022-05-19 15:13:14 +0000

    includes folder added

--- aspiring_devs/urls.py
+++ aspiring_devs/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.config import settings
+from django.config.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
